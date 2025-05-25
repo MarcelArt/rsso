f5b4c80d8666f883d3b340ed5f0e99a38a545360
MarcelArt <badman.islemon@gmail.com>
2025-05-25 23:41:08 +0700

    improve response with status code and using match expression

--- src/handlers/permission.rs
+++ src/handlers/permission.rs
@@ -1,38 +1,85 @@
-use actix_web::{delete, get, post, put, web::{self, Json}};
+use actix_web::{delete, get, post, put, web::{self, Json}, HttpResponse};
 
-use crate::{error::Error, models::permission::{Permission, PermissionDto}, repos::{self, base::IRepo}};
+use crate::{models::permission::PermissionDto, repos::{self, base::IRepo}};
 
 #[post("/")]
-pub async fn create(repo: web::Data<repos::Combined>, input: Json<PermissionDto>) -> Result<Json<Option<Permission>>, Error> {
-    let permission = repo.permission.create(input.0).await?;
+pub async fn create(repo: web::Data<repos::Combined>, input: Json<PermissionDto>) -> HttpResponse {
+    let permission = repo.permission.create(input.0).await;
 
-    Ok(Json(permission))
+    match permission {
+        Err(e) =>  {
+            return HttpResponse::InternalServerError().body(e.to_string());
+        },
+        Ok(None) => {
+            return HttpResponse::InternalServerError().body("Failed to create permission");
+        },
+        Ok(Some(permission)) => {
+            return HttpResponse::Created().json(permission);
+        }
+    }
 }
 
 #[get("/")]
-pub async fn read(repo: web::Data<repos::Combined>) -> Result<Json<Vec<Permission>>, Error> {
-    let permissions = repo.permission.read().await?;
+pub async fn read(repo: web::Data<repos::Combined>) -> HttpResponse {
+    let permissions = repo.permission.read().await;
 
-    Ok(Json(permissions))
+    match permissions {
+        Err(e) =>  {
+            return HttpResponse::InternalServerError().body(e.to_string());
+        },
+        Ok(permissions) => {
+            return HttpResponse::Ok().json(permissions);
+        }
+    }
 }
 
 #[put("/{id}")]
-pub async fn update(repo: web::Data<repos::Combined>, id: web::Path<String>, input: Json<PermissionDto>) -> Result<Json<Option<Permission>>, Error> {
-    let permission = repo.permission.update(id.into_inner(), input.0).await?;
+pub async fn update(repo: web::Data<repos::Combined>, id: web::Path<String>, input: Json<PermissionDto>) -> HttpResponse {
+    let permission = repo.permission.update(id.into_inner(), input.0).await;
 
-    Ok(Json(permission))
+    match permission {
+        Err(e) =>  {
+            return HttpResponse::InternalServerError().body(e.to_string());
+        },
+        Ok(None) => {
+            return HttpResponse::NotFound().finish();
+        },
+        Ok(Some(permission)) => {
+            return HttpResponse::Ok().json(permission);
+        }
+    }
 }
 
 #[delete("/{id}")]
-pub async fn delete(repo: web::Data<repos::Combined>, id: web::Path<String>) -> Result<Json<Option<Permission>>, Error> {
-    let permission = repo.permission.delete(id.into_inner()).await?;
+pub async fn delete(repo: web::Data<repos::Combined>, id: web::Path<String>) -> HttpResponse {
+    let permission = repo.permission.delete(id.into_inner()).await;
 
-    Ok(Json(permission))
+    match permission {
+        Err(e) =>  {
+            return HttpResponse::InternalServerError().body(e.to_string());
+        },
+        Ok(None) => {
+            return HttpResponse::NotFound().finish();
+        },
+        Ok(Some(permission)) => {
+            return HttpResponse::Ok().json(permission);
+        }
+    }
 }
 
 #[get("/{id}")]
-pub async fn get_by_id(repo: web::Data<repos::Combined>, id: web::Path<String>) -> Result<Json<Option<Permission>>, Error> {
-    let permission = repo.permission.get_by_id(id.into_inner()).await?;
+pub async fn get_by_id(repo: web::Data<repos::Combined>, id: web::Path<String>) -> HttpResponse {
+    let permission = repo.permission.get_by_id(id.into_inner()).await;
 
-    Ok(Json(permission))
+    match permission {
+        Err(e) =>  {
+            return HttpResponse::InternalServerError().body(e.to_string());
+        },
+        Ok(None) => {
+            return HttpResponse::NotFound().finish();
+        },
+        Ok(Some(permission)) => {
+            return HttpResponse::Ok().json(permission);
+        }
+    }
 }
--- src/handlers/user.rs
+++ src/handlers/user.rs
@@ -1,18 +1,35 @@
-use actix_web::{get, post, web::{self, Json}};
+use actix_web::{get, post, web::{self, Json}, HttpResponse};
 
-use crate::{error::Error, models::user::{User, UserDto}, repos::{self, base::IRepo}};
-
-#[get("/")]
-pub async fn read(repo: web::Data<repos::Combined>) -> Result<Json<Vec<User>>, Error> {
-    let users = repo.user.read().await?;
-    
-    Ok(Json(users))
-}
+use crate::{models::user::UserDto, repos::{self, base::IRepo}};
 
 #[post("/")]
-pub async fn create(repo: web::Data<repos::Combined>, user: Json<UserDto>) -> Result<Json<Option<User>>, Error> {    
+pub async fn create(repo: web::Data<repos::Combined>, user: Json<UserDto>) -> HttpResponse {    
     let user = user.0;
-    let user = repo.user.create(user).await?;
+    let user = repo.user.create(user).await;
 
-    Ok(Json(user))
+    match user {
+        Err(e) =>  {
+            return HttpResponse::InternalServerError().body(e.to_string());
+        },
+        Ok(None) => {
+            return HttpResponse::InternalServerError().body("Failed to create user");
+        },
+        Ok(Some(u)) => {
+            return HttpResponse::Created().json(u);
+        }
+    }
+}
+
+#[get("/")]
+pub async fn read(repo: web::Data<repos::Combined>) -> HttpResponse {
+    let users = repo.user.read().await;
+    
+    match users {
+        Err(e) =>  {
+            return HttpResponse::InternalServerError().body(e.to_string());
+        },
+        Ok(us) => {
+            return HttpResponse::Ok().json(us);
+        }
+    }
 }
